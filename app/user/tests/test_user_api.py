e1d4bfbb31eeaedb1271bac7f69cce2b0fd8203d
batuhanaltog <batuhanaltog88@gmail.com>
2024-07-15 22:38:42 +0300

    implementing token API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -86,6 +86,6 @@ class PublicUserApiTests(TestCase):
     def test_create_token_blank_password(self):
         """Test posting a blank password returns an error."""
         payload = {'email': 'test@example.com', 'password': ''}
-        res = self.client.pos(TOKEN_URL, payload)
+        res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
